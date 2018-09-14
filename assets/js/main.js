69f4ba889e446b8b759466c1ca1bf9d31754fbe8
vyshaal <vyshaal@ccs.neu.edu>
2018-09-14 13:15:25 -0400

    vyshaal-portfolio v1

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by vicky on 9/19/2017.
+ */
+$(document).ready(function () {
+    $(function(){
+        $("#profile-card-header").typed({
+            strings: ["Software Developer", "Data Scientist"],
+            typeSpeed: 0.3,
+            loop: true
+        });
+    });
+});
