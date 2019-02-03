4c4a6844f7316150fd8e437b4f63e11cd71a1871
vyshaal <vyshaal@ccs.neu.edu>
2019-02-03 01:21:14 -0500

    changes after joining insight

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 $(document).ready(function () {
     $(function(){
         $("#profile-card-header").typed({
-            strings: ["CS Graduate'18", "Software Developer", "Data Scientist"],
+            strings: ["CS Graduate'18", "Data Engineer", "Software Developer"],
             typeSpeed: 0.3,
             loop: true
         });
